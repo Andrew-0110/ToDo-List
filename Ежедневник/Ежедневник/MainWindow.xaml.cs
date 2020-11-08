4e1c350447fa903b4766f130df77ab40125bdb35
Andrew Baklanov <bdap5@mail.ru>
2020-11-08 13:54:46 +0300

    ToDo List
    ToDo List

---
+++ Ежедневник/Ежедневник/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ежедневник.Models;
+using Ежедневник.Services;
+
+namespace Ежедневник
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoData.json";
+        private BindingList<ToDoModel> toDoData;
+        private FileIOService fileIOService;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            fileIOService = new FileIOService(PATH);
+            try
+            {
+                toDoData = fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+            dtgTodoList.ItemsSource = toDoData;
+            toDoData.ListChanged += ToDoData_ListChanged;
+        }
+
+        private void ToDoData_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            switch (e.ListChangedType)
+            {
+                case ListChangedType.Reset:
+                    try
+                    {
+                        fileIOService.SaveData(sender);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        Close();
+                    }
+                    break;
+                case ListChangedType.ItemAdded:
+                    try
+                    {
+                        fileIOService.SaveData(sender);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        Close();
+                    }
+                    break;
+                case ListChangedType.ItemChanged:
+                    try
+                    {
+                        fileIOService.SaveData(sender);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        Close();
+                    }
+                    break;
+                case ListChangedType.ItemDeleted:
+                    try
+                    {
+                        fileIOService.SaveData(sender);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        Close();
+                    }
+                    break;
+                case ListChangedType.ItemMoved:
+                    try
+                    {
+                        fileIOService.SaveData(sender);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        Close();
+                    }
+                    break;
+                case ListChangedType.PropertyDescriptorAdded:
+                    break;
+                case ListChangedType.PropertyDescriptorChanged:
+                    break;
+                case ListChangedType.PropertyDescriptorDeleted:
+                    break;
+                default:
+                    break;
+             }
+        }
+    }
+}
---
+++ Ежедневник/Ежедневник/Models/ToDoModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ежедневник.Models
+{
+    class ToDoModel : INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        private bool isDone;
+        public bool IsDone
+        {
+            get { return isDone; }
+            set
+            {
+                if(isDone == value)
+                    return;
+                isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        private string text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                if(text == value)
+                    return;
+                text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
